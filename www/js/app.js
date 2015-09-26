2a7a3acf8ec92408ec78d14b01ef0e2e805a59ab
Equan Pr <equan.p@gmail.com>
2015-09-26 15:35:59 +0700

    Add setting ip and address form

--- www/js/app.js
+++ www/js/app.js
@@ -12,9 +12,9 @@ angular.module('ionic.utils', [])
     }
 }]);
 
-angular.module('ArduinoLed', ['ionic', 'ionic.utils'])
+var aled = angular.module('ArduinoLed', ['ionic', 'ionic.utils']);
 
-.run(function($ionicPlatform, $localstorage) {
+aled.run(function($ionicPlatform, $localstorage) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -24,11 +24,16 @@ angular.module('ArduinoLed', ['ionic', 'ionic.utils'])
     }
   });
 
-  $localstorage.set('ip', '192.168.0.100');
-  $localstorage.set('port','3000');
-
   var port = $localstorage.get('port');
+  if(!port) {
+    $localstorage.set('ip', '192.168.0.100');
+  }
+  
   var ip = $localstorage.get('ip');
+  if(!ip) {
+    $localstorage.set('port','3000');
+  }
+
   var iosocket = io.connect('http://'+ip+':'+port);
 
   $('#led').on('change', function(){
@@ -43,6 +48,41 @@ angular.module('ArduinoLed', ['ionic', 'ionic.utils'])
            $('#lampStatus').removeClass('ledColorOn').addClass('ledColorOff');
            iosocket.emit('button', {lampstatus: "0"});
         }
-  })
-});
+  });
+})
 
+aled.controller('SettingController', function($scope, $window, $ionicPopup, $timeout, $localstorage){
+  $scope.showPopup = function() {
+    $scope.data = {};
+    $scope.data.ip = $localstorage.get('ip');
+    $scope.data.port = $localstorage.get('port');
+
+    var myPopup = $ionicPopup.show({
+     template: '<div class="setform"><span>IP Address</span><input type="ip" ng-model="data.ip"><span>Port</span><input type="port" ng-model="data.port"></div>',
+     title: 'Settings',
+     scope: $scope,
+     buttons: [
+       { text: 'Cancel' },
+       {
+         text: '<b>Save</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           if (!$scope.data.ip) {
+             e.preventDefault();
+           } else {
+             $localstorage.set('ip', $scope.data.ip);
+             if($scope.data.port){
+                $localstorage.set('port', $scope.data.port); 
+             }
+             $window.location.reload(true);
+             return $scope.data.ip;
+           }
+         }
+       },
+     ]
+   });
+   myPopup.then(function(res) {
+     console.log('Tapped!', res);
+   });
+  };
+})
